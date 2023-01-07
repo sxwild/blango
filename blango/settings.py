3a38e8fd2290f4ad966431a813bb8b9eb656c831
Simon Wild <simon@wilderness.ink>
2023-01-07 05:27:15 +0000

    Finish crispy forms

--- blango/settings.py
+++ blango/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd Party
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     # applications
     'blog',
 ]
@@ -134,3 +138,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
